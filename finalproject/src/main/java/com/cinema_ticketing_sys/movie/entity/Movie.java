977e70dcd0a154fabae8d4cbcd8236a234a1f852
wangzi <785692920@qq.com>
2017-06-03 19:07:06 +0800

    update from huitr

--- finalproject/src/main/java/com/cinema_ticketing_sys/movie/entity/Movie.java
+++ finalproject/src/main/java/com/cinema_ticketing_sys/movie/entity/Movie.java
@@ -8,23 +8,30 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
 /**
- * Created by huitianrui on 2017/5/11.
+ * Created by huitianrui on 2017/6/3.
  */
 @Entity
-@Table(name = "movie", schema = "cinema_ticketing_db")
 public class Movie {
     private int movieId;
     private String movieName;
     private String description;
     private Double price;
     private String posterUrl;
+    private String director;
+    private String cast;
+    private String type;
+    private String region;
+    private String language;
+    private Integer length;
 
     private Set<MovieCutting> movieCuttings = new HashSet<>();
     private Set<Comment> comments = new HashSet<>();
     private Cinema cinema;
 
     public Movie() {}
+
 
     // 电影 双向一对多关联 评论
     @OneToMany(cascade = CascadeType.ALL, targetEntity = Comment.class, mappedBy = "movie")
@@ -57,8 +64,9 @@ public class Movie {
         this.movieCuttings = movieCuttings;
     }
 
+
     @Id
-    @Column(name = "movie_id")
+    @Column(name = "movie_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getMovieId() {
         return movieId;
@@ -69,7 +77,7 @@ public class Movie {
     }
 
     @Basic
-    @Column(name = "movie_name")
+    @Column(name = "movie_name", nullable = true, length = 45)
     public String getMovieName() {
         return movieName;
     }
@@ -79,7 +87,7 @@ public class Movie {
     }
 
     @Basic
-    @Column(name = "description")
+    @Column(name = "description", nullable = true, length = -1)
     public String getDescription() {
         return description;
     }
@@ -89,7 +97,7 @@ public class Movie {
     }
 
     @Basic
-    @Column(name = "price")
+    @Column(name = "price", nullable = true, precision = 0)
     public Double getPrice() {
         return price;
     }
@@ -99,13 +107,73 @@ public class Movie {
     }
 
     @Basic
-    @Column(name = "poster_url")
+    @Column(name = "poster_url", nullable = true, length = 45)
     public String getPosterUrl() {
         return posterUrl;
     }
 
     public void setPosterUrl(String posterUrl) {
         this.posterUrl = posterUrl;
+    }
+
+    @Basic
+    @Column(name = "director", nullable = true, length = 45)
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Basic
+    @Column(name = "cast", nullable = true, length = 100)
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    @Basic
+    @Column(name = "type", nullable = true, length = 45)
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Basic
+    @Column(name = "region", nullable = true, length = 45)
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @Basic
+    @Column(name = "language", nullable = true, length = 45)
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Basic
+    @Column(name = "length", nullable = true)
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
     }
 
     @Override
@@ -116,25 +184,33 @@ public class Movie {
         Movie movie = (Movie) o;
 
         if (movieId != movie.movieId) return false;
-        if (!movieName.equals(movie.movieName)) return false;
-        if (!description.equals(movie.description)) return false;
-        if (!price.equals(movie.price)) return false;
-        if (!posterUrl.equals(movie.posterUrl)) return false;
-        if (!movieCuttings.equals(movie.movieCuttings)) return false;
-        if (!comments.equals(movie.comments)) return false;
-        return cinema.equals(movie.cinema);
+        if (movieName != null ? !movieName.equals(movie.movieName) : movie.movieName != null) return false;
+        if (description != null ? !description.equals(movie.description) : movie.description != null) return false;
+        if (price != null ? !price.equals(movie.price) : movie.price != null) return false;
+        if (posterUrl != null ? !posterUrl.equals(movie.posterUrl) : movie.posterUrl != null) return false;
+        if (director != null ? !director.equals(movie.director) : movie.director != null) return false;
+        if (cast != null ? !cast.equals(movie.cast) : movie.cast != null) return false;
+        if (type != null ? !type.equals(movie.type) : movie.type != null) return false;
+        if (region != null ? !region.equals(movie.region) : movie.region != null) return false;
+        if (language != null ? !language.equals(movie.language) : movie.language != null) return false;
+        if (length != null ? !length.equals(movie.length) : movie.length != null) return false;
+
+        return true;
     }
 
     @Override
     public int hashCode() {
         int result = movieId;
-        result = 31 * result + movieName.hashCode();
-        result = 31 * result + description.hashCode();
-        result = 31 * result + price.hashCode();
-        result = 31 * result + posterUrl.hashCode();
-        result = 31 * result + movieCuttings.hashCode();
-        result = 31 * result + comments.hashCode();
-        result = 31 * result + cinema.hashCode();
+        result = 31 * result + (movieName != null ? movieName.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (posterUrl != null ? posterUrl.hashCode() : 0);
+        result = 31 * result + (director != null ? director.hashCode() : 0);
+        result = 31 * result + (cast != null ? cast.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (region != null ? region.hashCode() : 0);
+        result = 31 * result + (language != null ? language.hashCode() : 0);
+        result = 31 * result + (length != null ? length.hashCode() : 0);
         return result;
     }
 }
